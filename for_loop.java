3f768ecc14f11a92a34fed0d305cd1ab27e3904a
karan soni <karanparekh1011@gmail.com>
2025-03-01 23:39:48 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,15 @@
+/*for loop is a control flow statement used to repeatedly execute a block of code a specific number of times. 
+syntax:
+for(initialization, condition, update){}
+
+1. Standard loop for running code multiple times.
+2. Generally preferred for counting iterations.
+ */
+public class for_loop {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
