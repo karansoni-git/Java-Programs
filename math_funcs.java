15f08d99acd21a551815d977a5ca3b63d5bcae1c
karan soni <karanparekh1011@gmail.com>
2025-03-03 09:52:22 +0530

    Add files via upload

---
+++ math_funcs.java
@@ -0,0 +1,17 @@
+/* The Java Math class has many methods that allows you to perform mathematical tasks on numbers. 
+Note: All Math methods are static.*/
+public class math_funcs{
+    public static void main(String[] args) {
+        System.out.println("max : "+ Math.max(38,48));
+        System.out.println("min : "+ Math.min(38,48));
+        System.out.println("pow : "+ Math.pow(2,4));
+        System.out.println("sqrt : "+ Math.sqrt(100));
+        System.out.println("ceil : "+ Math.ceil(54.65));
+        System.out.println("floor : "+ Math.floor(38.89));
+        System.out.println("round : "+ Math.round(61.65));
+        System.out.println("random : "+ Math.random());//generate number from 0-1.
+        System.out.println("absolute : "+ Math.abs(-195));
+        System.out.println("cbrt : "+ Math.cbrt(1000));
+        //not only these are there are so many more functions available in math class.
+    }
+}
