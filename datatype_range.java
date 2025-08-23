d6b7ec9b727f15765d73c13cbd62de2b5b2de9f1
karan soni <karanparekh1011@gmail.com>
2025-08-23 23:44:21 +0530

    Add files via upload

---
+++ datatype_range.java
@@ -0,0 +1,21 @@
+public class datatype_range {
+    public static void main(String[] args) {
+        System.out.println("Integral Data Type Ranges : ");
+        System.out.println("Byte Minimum Value : " + Byte.MIN_VALUE);
+        System.out.println("Byte Maximum Value : " + Byte.MAX_VALUE);
+        System.out.println("Short Minimum Value : " + Short.MIN_VALUE);
+        System.out.println("Short Maximum Value : " + Short.MAX_VALUE);
+        System.out.println("Integer Minimum Value : " + Integer.MIN_VALUE);
+        System.out.println("Integer Maximum Value : " + Integer.MAX_VALUE);
+        System.out.println("Long Minimum Value: " + Long.MIN_VALUE);
+        System.out.println("Long Maximum Value: " + Long.MAX_VALUE);
+        System.out.println("\nDecimal Data Type Ranges : ");
+        System.out.println("Float Minimum Value : " + Float.MIN_VALUE);
+        System.out.println("Float Maximum Value : " + Float.MAX_VALUE);
+        System.out.println("Double Minimum Value : " + Double.MIN_VALUE);
+        System.out.println("Double Maximum Value : " + Double.MAX_VALUE);
+        System.out.println("\nCharacter Data Type Range : ");
+        System.out.println("Character Minimum Value : " + (int) Character.MIN_VALUE);
+        System.out.println("Character Maximum Value : " + (int) Character.MAX_VALUE);
+    }
+}
